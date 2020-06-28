6dd453a16c83e76596abedfe512e13310b794602
Zane Hitchcox <zwhitchcox@gmail.com>
2020-06-28 08:12:51 -0400

    update homepages and prepublish info

---
+++ _scripts/publish-libs.ps1
@@ -0,0 +1,23 @@
+cd field
+npm version patch
+yarn publish
+
+cd ../input
+npm version patch
+yarn publish
+
+cd ../input-basic
+npm version patch
+yarn publish
+
+cd ../input-mui
+npm version patch
+yarn publish
+
+cd ../input-picker
+npm version patch
+yarn publish
+
+cd ../validate
+npm version patch
+yarn publish
